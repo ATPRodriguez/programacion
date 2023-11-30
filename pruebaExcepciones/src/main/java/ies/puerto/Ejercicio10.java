e078328e89fa99b31c11d88d9bf3519598c1380e
ATPRodriguez <alejandrotomaspachecorodriguez@gmail.com>
2023-11-30 14:50:50 +0000

    Añadido InvalidNameException

--- pruebaExcepciones/src/main/java/ies/puerto/Ejercicio10.java
+++ pruebaExcepciones/src/main/java/ies/puerto/Ejercicio10.java
@@ -1,16 +1,21 @@
 package ies.puerto;
 
+
+/**
+ * Crea una excepción personalizada llamada NombreInvalidoException que se lance cuando un método reciba
+ * un nombre nulo o vacío como argumento. Implementa una clase que contenga un método
+ * que acepte un nombre y lance esta excepción si es inválido.
+ * @author ATPRodriguez
+ */
 public class Ejercicio10 {
-
-    public static void main(String[] args) throws MiException {
-        boolean verdadero = true;
-
-        metodoConCondicion(verdadero);
+    public static void main(String[] args) throws InvalidNameException {
+        boolean condition = true;
+        nombre(condition);
     }
 
-    public static void metodoConCondicion(boolean verdadero) throws MiException {
-        if (verdadero) {
-            throw new MiException(1,"MiException lanza un mensaje");
+    public static void nombre(boolean condition) throws InvalidNameException {
+        if (condition){
+            throw new InvalidNameException(1, "InvalidNameException shows a message");
         }
     }
 }
