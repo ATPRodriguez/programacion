ee796ae05ef5091610537b52365ee4c0aa0590f4
ATPRodriguez <alejandrotomaspachecorodriguez@gmail.com>
2023-12-14 18:27:34 +0000

    Hecho Ejercicio1

--- Tarea3Eval/src/main/java/ies/puerto/Persona.java
+++ Tarea3Eval/src/main/java/ies/puerto/Persona.java
@@ -24,11 +24,21 @@ public class Persona {
         long edad = (Math.abs(millisActual - millisNacer) / (1000*60*60*24))/365;
         // Solo lo relaciona con los años
         System.out.println(edad);
+    }
 
+    public String obtenerSignoZodiacal () {
+        int fecha = (int) (this.fechaNacimiento.getTime()* 1000 * 60 * 60 *24)/365;
+        switch (fecha) {
+            case 20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49:
+                return "Acuario";
+            default:
+                return "Capricornio";
+        }
     }
 
     public static void main(String[] args) throws ParseException {
-        Persona persona = new Persona("Alex", "15/12/2003");
+        Persona persona = new Persona("Alex", "02/02/2003");
         persona.calcularEdad();
+        System.out.println(persona.obtenerSignoZodiacal());
     }
 }
