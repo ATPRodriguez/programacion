d5d4dec5aee9a14b4a79cfd6192b2e8622202323
AlejandroTomas <alu0101472164@alu.edu.es>
2024-01-20 18:26:02 +0000

    Terminado EjerciciosInterfaz

--- EjerciciosInterfaz/src/main/java/ies/puerto/app/AppPersona.java
+++ EjerciciosInterfaz/src/main/java/ies/puerto/app/AppPersona.java
@@ -11,10 +11,13 @@ public class AppPersona {
     static Profesor profesor;
 
     public static void main(String[] args) {
-        profesor = new Profesor("Pedro", "28/05/2000", 1000f, "Matematicas");
-        alumno = new Alumno("Juan", "19/02/2013", "1º CFGS");
+        profesor = new Profesor("Pedro", "28/05/1985", 1000f, "Matematicas");
+        alumno = new Alumno("Juan", "19/02/2007", "1º BACH");
 
         System.out.println(profesor.toString());
+        System.out.println(profesor.impartirClase());
+        System.out.println();
+
         System.out.println(alumno.toString());
     }
 }
